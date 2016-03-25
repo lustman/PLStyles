cbee8a9d60fbb51910b1c34c6471db26f83624c5
shikiyoku <ustnihc@gmail.com>
2016-03-25 22:00:34 +0300

    Initial commit. Almost all work is done. There are might be some missed places

---
+++ Gulpfile.js
@@ -0,0 +1,22 @@
+﻿var gulp = require('gulp');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var colorguard = require('gulp-colorguard');
+
+gulp.task('styles', function() {
+   var processors = [
+         autoprefixer({browsers: ['last 2 versions']}),
+         colorguard
+       ];
+  
+  gulp.src('sass/**/*.scss')
+      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(postcss(processors))
+      .pipe(gulp.dest('./css/'));
+});
+
+// watch task
+gulp.task('default', function() {
+   gulp.watch('sass/**/*.scss', ['styles']);
+});
